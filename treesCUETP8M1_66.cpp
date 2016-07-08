b1e4bd4608af91fcd386ec2e16c942b3f73d8b9e
hanweiz <h.w.ang@u.nus.edu>
2016-07-09 00:38:39 +0800

    Add files via upload

--- treesCUETP8M1_66.cpp
+++ treesCUETP8M1_66.cpp
@@ -46,11 +46,9 @@ void treesCUETP8M1_66()
     tree->SetBranchAddress("recoTracksp4", &reco_tracks);
     //vector<TLorentzVector> *tracks = new vector<TLorentzVector>;
 
-    vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > > *gen_tracks = 0;
-    tree->SetBranchAddress("genParticlesp4", &gen_tracks);
-
     vector<float> *fVec_dz = 0;
     tree->SetBranchAddress ("recoTracksdz", &fVec_dz);
+
     vector<float> *fVec_dzErr = 0;
     tree->SetBranchAddress ("recoTracksdzErr", &fVec_dzErr);
 
@@ -65,31 +63,76 @@ void treesCUETP8M1_66()
     vector<int> *nVec_HighPurity = 0;
     tree->SetBranchAddress ("recoTrackshighPurity", &nVec_HighPurity);
 
+    vector<float> *fvec_reco_Vtxy = 0; //require initialisation to 0 to avoid crash
+    tree->SetBranchAddress("vtxy",&fvec_reco_Vtxy);
+
+	vector<float> *fvec_reco_Vtxx = 0; //require initialisation to 0 to avoid crash
+    tree->SetBranchAddress("vtxx",&fvec_reco_Vtxx);
+
+    double dzleaf = 0;
+	double dzcalc = 0;
+	double dzminus= 0;
+
+	double d0leaf = 0;
+	double d0calc = 0;
+	double d0minus = 0;
+
+
     //declare variable to hold track number
-    int ntrk, nMulti;
+    int ntrk_reco_total,
+    double d_reco_trk;
+    double d_reco_evt = 0;
     int ntrk_normalized = 0;
     int nHigh_Purity = 0;
-    float dz_dzErr, d0_d0Err, pt_ptErr;
+    float dz_dzErr, d0_d0Err, ptErr_pt;
+
+//---------------------Histograms for reco Tracks-------------------------------------------
+
+    TCanvas *reco_canvas = new TCanvas;
+    TCanvas *gen_canvas = new TCanvas;
+    //TH1F *event_histo = new TH1F ("reco_evt", "reco_evt", 100, 0, 200);
+    TH1F *reco_pt_histo = new TH1F ("reco_pt", "Normalized_reco_pT", 100, 0, 100);
+    TH1F *reco_eta_histo = new TH1F ("reco_eta", "Normalized_reco_Eta", 25, -3, 3);
+    TH1F *reco_phi_histo = new TH1F ("reco_phi", "Normalized_reco_Phi", 100, -4, 4);
+    TH1F *lumi_histo = new TH1F ("lumi_section", "lumi_section", 160, 80, 230);
+    TH1F *reco_multiplicity = new TH1F ("reco_multiplicity", "Normalized_reco_Multiplicity", 200, 0, 200);
+    TH1F *dz_sigmadz = new TH1F ("dz_sigmadz", "Normalized_dz_sigmadz", 160, -20, 20);
+    TH1F *d0_sigmad0 = new TH1F ("d0_sigmad0", "Normalized_d0_sigmad0", 160, -20, 20);
+    TH1F *sigmapt_pt = new TH1F ("sigmapt_pt", "Normalized_sigmapt_pt", 20, 0, 0.2);
+    //TH1F *normalized_multiplicity_histo = new TH1F ("normalized_multiplicity", "normalized_multiplicity", 200, 0, 200);
+
+//---------------------Variables for gen tracks-------------------------------------------
+
+    float *f_gen_Vtxz = 0;
+    tree->SetBranchAddress("simvtxz",&f_gen_Vtxz);
+
+    vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > > *gen_tracks = 0;
+    tree->SetBranchAddress("genParticlesp4", &gen_tracks);
+    //vector<TLorentzVector> *tracks = new vector<TLorentzVector>;
+
+    int ntrk_gen, ngen_Multi;
+
+//---------------------Histograms for gen tracks-------------------------------------------
+
+    TH1F *gen_pt_histo = new TH1F ("gen_pt", "Normalized_gen_pT", 100, 0, 100);
+    TH1F *gen_eta_histo = new TH1F ("gen_eta", "Normalized_gen_Eta", 25, -3, 3);
+    TH1F *gen_phi_histo = new TH1F ("gen_phi", "Normalized_gen_Phi", 100, -4, 4);
+    TH1F *gen_multiplicity = new TH1F ("gen_multiplicity", "Normalized_Multiplicity", 200, 0, 200);
+
+//---------------------Efficiency-------------------------------------
+
+    TH1F *pt_efficiency = new TH1F("pt_efficiency", "pt efficiency", 100, 0, 200);
+    TH1F *eta_efficiency = new TH1F("eta_efficiency", "eta efficiency", 100, 0, 200);
+    TH1F *phi_efficiency = new TH1F("phi_efficiency", "phi efficiency", 100, 0, 200);
+    float fpt_eff, feta_eff, fphi_eff;
+
+//---------------------Cuts-------------------------------------------
 
     const int lumi_cut = 90;
     const double eta_cut = 2.4;
     const double pt_cut = 0.5;
     const double vtxz_number = 1.;
     const double vtxz_size = 10;
-    const int ntracks = 533084;
-
-
-    TCanvas *canvas = new TCanvas;
-    //TH1F *event_histo = new TH1F ("reco_evt", "reco_evt", 100, 0, 200);
-    TH1F *pt_histo = new TH1F ("reco_pt", "Normalized_reco_pT", 100, 0, 100);
-    TH1F *eta_histo = new TH1F ("reco_eta", "Normalized_reco_Eta", 25, -3, 3);
-    TH1F *phi_histo = new TH1F ("reco_phi", "Normalized_reco_Phi", 100, -4, 4);
-    TH1F *lumi_histo = new TH1F ("lumi_section", "lumi_section", 160, 80, 230);
-    TH1F *multiplicity = new TH1F ("multiplicity", "Normalized_Multiplicity", 200, 0, 200);
-    TH1F *dz_sigmadz = new TH1F ("dz_sigmadz", "Normalized_dz_sigmadz", 200, 0, 200);
-    TH1F *d0_sigmad0 = new TH1F ("d0_sigmad0", "Normalized_d0_sigmad0", 200, 0, 200);
-    TH1F *pt_sigmapt = new TH1F ("pt_sigmapt", "Normalized_pt_sigmapt", 200, 0, 200);
-    //TH1F *normalized_multiplicity_histo = new TH1F ("normalized_multiplicity", "normalized_multiplicity", 200, 0, 200);
 
     //select events with only 1 vertex using information from Branch 46/47/48. Here we use Branch 48 (z-axis)
     Int_t nEvt = (Int_t)tree->GetEntries();
@@ -103,7 +146,6 @@ void treesCUETP8M1_66()
             //cout<< "Processing event number: " << i << endl; //1 TTree entry = 1 event
         cout << "Entry " << i << endl;
         tree -> GetEntry(i);
-        cout << "set branch address for zero bias" << endl;
 
         //selects for events from lumisection >= 90
         if (nlumi_section >= lumi_cut)
@@ -118,9 +160,32 @@ void treesCUETP8M1_66()
                 fVec_reco_Vtxz_size = fVec_reco_Vtxz->size();
                 //vtxz_plot->Fill(fvecVtxz_size);
 
+//---------------------Loops for gen tracks-------------------------------------------
+
+                ntrk_gen = gen_tracks->size();
+                ngen_Multi = 0;
+
+                //looping over generated tracks
+                for(int q = 0; q != ntrk_gen; ++q)
+                {
+                    XYZTVector gen_vec = (*gen_tracks)[q];
+
+                    if (abs (gen_vec.Eta()) <= eta_cut && gen_vec.Pt() >= pt_cut)
+                    {
+                        gen_phi_histo->Fill(gen_vec.Phi());
+                        gen_pt_histo->Fill(gen_vec.Pt());
+                        gen_eta_histo->Fill(gen_vec.Eta());
+                        ++ngen_Multi;
+                    }
+                }
+                gen_multiplicity->Fill(ngen_Multi);
+
+
+//---------------------Loops for reco tracks-------------------------------------------
+
                 if (fVec_reco_Vtxz_size == vtxz_number)
                 {
-                    cout << "number of vertex for event " << i << " is " << fVec_reco_Vtxz_size << endl;
+                    //cout << "number of vertex for event " << i << " is " << fVec_reco_Vtxz_size << endl;
 
                     //looping over vertices
                     for (int k = 0; k != fVec_reco_Vtxz_size; ++k)
@@ -130,32 +195,53 @@ void treesCUETP8M1_66()
 
                         if (n_reco_BeamSize <= vtxz_size)
                         {
-                            ntrk = reco_tracks->size();
+                            ntrk_reco_total = reco_tracks->size();
 
                             //fill with the lumi sections that meet the above event-level cuts
                             lumi_histo->Fill(nlumi_section);
 
-                            nMulti = 0;
+                            d_reco_trk = 0;
+                            ++d_reco_evt;
+
 
                             //looping over tracks
-                            for (int j = 0; j != ntrk; ++j)
+                            for (int j = 0; j != ntrk_reco_total; ++j)
                             {
-                                XYZTVector vec = (*reco_tracks)[j];
-                                dz_dzErr = ((*fVec_dz)[j])/((*fVec_dzErr)[j]);
-                                d0_d0Err = ((*fVec_d0)[j])/((*fVec_d0Err)[j]);
-                                pt_ptErr = ((vec.Pt())/(*fVec_ptErr)[j]);
+                                XYZTVector reco_vec = (*reco_tracks)[j];
+
+                                if (j == 9)
+								{
+									//dzleaf = (*fVec_dz)[j];
+									dzcalc = ((reco_vec.Z())-(*fVec_reco_Vtxz)[k])-((((reco_vec.X())-(*fvec_reco_Vtxx)[k])*(reco_vec.px())+((reco_vec.Y())-(*fvec_reco_Vtxy)[k])*(reco_vec.py()))/(reco_vec.Pt())*(reco_vec.pz()/reco_vec.Pt()));
+									//dzminus = ((vec.Z())-(*fvecVtxz)[k]);
+                                    //tr_d0= (- (tr_x-vtx_x)  track.py() + (tr_y-vtx_y)  track.px() ) / track.pt()
+									d0leaf = (*fVec_d0)[j];
+									//d0calc = ((-(reco_vec.X() - (*fvec_reco_Vtxx)[k])*reco_vec.Py()) + ((reco_vec.Y()-(*fvec_reco_Vtxy)[k])*reco_vec.Px()))/reco_vec.Pt();
+                                    d0calc = ((reco_vec.Y() - (*fvec_reco_Vtxy)[k])*reco_vec.Px() - (reco_vec.X() - (*fvec_reco_Vtxx)[k])*reco_vec.Py())/reco_vec.Pt();
+								}
 
                                 if ((*nVec_HighPurity)[j] == 1)
                                 {
-                                    if (abs (vec.Eta()) <= eta_cut && vec.Pt() >= pt_cut)
+                                    if (abs (reco_vec.Eta()) <= eta_cut && reco_vec.Pt() >= pt_cut)
                                     {
-                                        phi_histo->Fill(vec.Phi());
-                                        pt_histo->Fill(vec.Pt());
-                                        eta_histo->Fill(vec.Eta());
+                                        dz_dzErr = ((*fVec_dz)[j])/((*fVec_dzErr)[j]);
+                                        d0_d0Err = ((*fVec_d0)[j])/((*fVec_d0Err)[j]);
+                                        ptErr_pt = ((*fVec_ptErr)[j]/(reco_vec.Pt()));
+
+                                        //fphi_eff = reco_vec.Phi()/gen_vec.Phi();
+                                        //feta_eff = reco_vec.Eta()/gen_vec.Eta();
+                                        //fpt_eff = reco_vec.Pt()/gen_vec.Pt();
+
+                                        pt_efficiency->Fill(fpt_eff);
+                                        eta_efficiency->Fill(feta_eff);
+                                        phi_efficiency->Fill(fphi_eff);
+                                        reco_phi_histo->Fill(reco_vec.Phi());
+                                        reco_pt_histo->Fill(reco_vec.Pt());
+                                        reco_eta_histo->Fill(reco_vec.Eta());
                                         dz_sigmadz->Fill(dz_dzErr);
                                         d0_sigmad0->Fill(d0_d0Err);
-                                        pt_sigmapt->Fill(pt_ptErr);
-                                        ++nMulti;
+                                        sigmapt_pt->Fill(ptErr_pt);
+                                        ++d_reco_trk;
                                     }
                                     ++nHigh_Purity;
                                 }
@@ -175,8 +261,8 @@ void treesCUETP8M1_66()
 
                             }
 
-                            multiplicity->Fill(nMulti);
-                            ntrk_normalized += nMulti;
+                            reco_multiplicity->Fill(d_reco_trk);
+                            ntrk_normalized += d_reco_trk;
                         }
 
 
@@ -190,54 +276,82 @@ void treesCUETP8M1_66()
 
     }
 
-    cout << "Total number of selected tracks is " << ntrk_normalized << endl;
+    //cout << "Total number of selected tracks is " << ntrk_normalized << endl;
+    cout << "d0leaf is " << d0leaf << " d0calc is "<< d0calc << endl;
+    cout << "Total number of selected events is " << d_reco_evt << endl;
+    cout << "Total number of selected tracks is " << d_reco_trk << endl;
 
-//vtxz_plot->Draw();
-//canvas->Update();
-//canvas->SaveAs("vtxz_number.png");
-//delete canvas;
-//delete vtxz_plot;
+    //vtxz_plot->Draw();
+    //reco_canvas->Update();
+    //reco_canvas->SaveAs("vtxz_number.png");
+    //delete reco_canvas;
+    //delete vtxz_plot;
 
 
     //after the loop over all events, draw the resulting plots
 
-    canvas->Divide(3,2);
-    /*canvas->cd(1);
+    reco_canvas->Divide(3,2);
+
+    reco_canvas->cd(1);
+    gPad->SetLogy();
+    gPad->SetLogx();
+    reco_pt_histo->Scale(1/n_reco_evt);
+    reco_pt_histo->Draw();
+    gen_pt_histo->SetLineColor(kRed-2);
+    gen_pt_histo->Scale(1/n_reco_evt, "SAME");
+    gen_pt_histo->Draw();
+
+    reco_canvas->cd(2);
+    gPad->SetLogy();
+    gPad->SetLogx();
+    reco_eta_histo->Scale(1/n_reco_evt);
+    reco_eta_histo->Draw();
+    gen_eta_histo->SetLineColor(kBlue-4);
+    gen_eta_histo->Scale(1/n_reco_evt, "SAME");
+    gen_eta_histo->Draw();
+
+    reco_canvas->cd(3);
+    gPad->SetLogy();
+    reco_phi_histo->Scale(1/n_reco_evt);
+    reco_phi_histo->Draw();
+    gen_phi_histo->SetLineColor(kGreen);
+    gen_phi_histo->Scale(1/n_reco_evt, "SAME");
+    gen_phi_histo->Draw();
+
+    /*reco_canvas->cd(4);
     gPad->SetLogy();
     dz_sigmadz->DrawNormalized("", 1);
 
-    canvas->cd(2);
+    reco_canvas->cd(5);
     gPad->SetLogy();
     d0_sigmad0->DrawNormalized("", 1);
 
+    reco_canvas->cd(6);
     gPad->SetLogy();
-    pt_sigmapt->DrawNormalized("", 1);
-    canvas->Update();*/
+    sigmapt_pt->DrawNormalized("", 1);*/
+    reco_canvas->Update();
+    reco_canvas->SaveAs("treesCUETP8M1_66_relative_errors_compare.pdf");
 
-    canvas->cd(1);
+    /*gen_canvas->Divide(2,2);
+
+    gen_canvas->cd(1);
     gPad->SetLogy();
-    pt_histo->DrawNormalized("", 1);
+    gen_multiplicity->SetLineColor(kBlue+2);
+    gen_multiplicity->DrawNormalized("", 1);
+    reco_multiplicity->SetLineColor(kBlack);
+    reco_multiplicity->DrawNormalized("SAME", 1);
 
-    canvas->cd(2);
-    gPad->SetLogy();
-    eta_histo->DrawNormalized("", 1);
+    gen_canvas->cd(2);
+    pt_efficiency->Draw();
 
-    canvas->cd(3);
-    gPad->SetLogy();
-    phi_histo->DrawNormalized("", 1);
+    gen_canvas->cd(3);
+    eta_efficiency->Draw();
 
-    canvas->cd(4);
-    gPad->SetLogy();
-    dz_sigmadz->DrawNormalized("", 1);
+    gen_canvas->cd(4);
+    phi_efficiency->Draw();
 
-    canvas->cd(5);
-    gPad->SetLogy();
-    d0_sigmad0->DrawNormalized("", 1);
+    gen_canvas->Update();
+    gen_canvas->SaveAs("multiplicity_efficiency_plots_compare.pdf");*/
 
-    canvas->cd(6);
-    gPad->SetLogy();
-    pt_sigmapt->DrawNormalized("", 1);
-    canvas->Update();
 
-    canvas->SaveAs("treesCUETP8M1_66_reco_relative_errors.pdf");
 }
